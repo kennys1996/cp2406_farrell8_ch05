4d1d909678b116205874dd8e7aae36cc2467d7c4
PanTianke <38001570+kennys1996@users.noreply.github.com>
2018-08-06 14:02:50 +0800

    Add files via upload

--- DebuggingExercises/DebugFive1.java
+++ DebuggingExercises/DebugFive1.java
@@ -15,18 +15,18 @@ public class DebugFive1
       int usersChoice;
       double bill = 0.0;
       usersChoiceString = JOptionPane.showInputDialog(null,
-         "Order please\n1 - Burger\n2 - Hotdog" +
-         "\n3 - Grilled cheese\n4 - Fish sandwich");
-      usersChoice= Integer.parseInt(usersChoice);
-      if(usersChoice == 1 && usersChoice == 2)
-         bill = bill + LOW_PRICE;
+              "Order please\n1 - Burger\n2 - Hotdog" +
+                      "\n3 - Grilled cheese\n4 - Fish sandwich");
+      usersChoice= Integer.parseInt(usersChoiceString);
+      if(usersChoice == 1 || usersChoice == 2)
+         bill = bill + HIGH_PRICE;
       else
-         bill = bill - MED_PRICE;
+         bill = bill + MED_PRICE;
       usersChoiceString = JOptionPane.showInputDialog(null,
-          "Fries with that?\n1 - Yes\n2 - No");
-      usersChoice = Integer.parse(usersChoiceString);
-      if (usersChoice == 1);
-          bill = bill + LOW_PRICE;
+              "Fries with that?\n1 - Yes\n2 - No");
+      usersChoice = Integer.parseInt(usersChoiceString);
+      if (usersChoice == 1)
+         bill = bill + LOW_PRICE;
       JOptionPane.showMessageDialog(null,"Bill is " + bill);
    }
 }
